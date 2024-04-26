ab7cd8c5923e7cad8a2452fe98ec1aced94ce492
Greta Wall <greta.wall@kellogg.northwestern.edu>
2024-04-26 14:50:49 +0000

    first changes

---
+++ app/models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ApplicationRecord
+t.string "name"
+t.string "city"
+t.string "state"
+t.string "url"
+end
---
+++ app/models/salesperson.rb
@@ -0,0 +1,4 @@
+class Salesperson < ApplicationRecord
+    #talk to the table, this "model" slash factory creates each salesperson?
+    
+end
